e25571630a3360f2e4b34aa3a8c40a6c3c05289b
KushPatel2006 <76569745+KushPatel2006@users.noreply.github.com>
2021-12-29 14:08:55 -0500

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,26 @@
+import random
+
+print("Welcome to Rahauls's number guessing game!\n")
+
+guesses = 0
+number = random.randint(0, 10)
+
+
+while( guesses < 3 ):
+    u_guess = int(input("Have a guess: "))
+
+    if u_guess == number:
+        print("Well done, you win!")
+        break
+    elif u_guess > number:
+        print("Your guess was too high.")
+        guesses = guesses + 1
+    elif u_guess < number:
+        print("Your guess was too low.")
+        guesses = guesses + 1
+    else:
+        print("There was an error.")
+
+if guesses == 3:
+    print("You lose!")
+    print("The number was: " + str(number))
